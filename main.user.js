1abcf9f82f300e3c0325d09e59e24cba518f5972
muimi <tj_ych@hotmail.com>
2013-06-08 17:41:00 +0800

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -17,7 +17,9 @@
 
 var script = document.createElement('script');
 script.id = "Honooon_script";
-if (location.host == "fenxiang.qq.com") {
+if (location.host == "http://kuai.xunlei.com/") {
+	script.src = "https://raw.github.com/muimi/aria2-browser-userscript/master/ThunderKCExporter.js";
+} else if (location.host == "fenxiang.qq.com") {
 	script.src = "https://raw.github.com/chztv/QQFXExporter/master/QQFXExporter.js";
 } else if (location.host == "lixian.qq.com") {
 	script.src = "https://raw.github.com/ondh/LixianExporter/master/QQLixianExport.js";
