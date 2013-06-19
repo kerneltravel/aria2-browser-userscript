f1144735fce40838923eb78700f78641c213fc0e
muimi <tj_ych@hotmail.com>
2013-06-19 20:25:21 +0800

    Update kuaiexporter.js

--- kuaiexporter.js
+++ kuaiexporter.js
@@ -1,10 +1,9 @@
 // ==UserScript==
-// @name        批量复制迅雷快传下载地址
+// @name        迅雷快传
 // @namespace   http://www.ualberta.ca/~xiaohui6
-// @version     0.4.25
 // @description 快速批量复制迅雷快传下载地址，直接复制到离线下载即可。
-// @match       http://kuai.xunlei.com/d/*
-// @copyright   2012+, Grey
+// @version     0.4.25
+// @include     http://kuai.xunlei.com/d/*
 // ==/UserScript==
 
 function getQueryStringArgs(qs){
@@ -23,18 +22,43 @@ function getQueryStringArgs(qs){
 }
 
 function getUrls() {
-  var urls = "";
+	var urls = "";
 	var files = document.getElementsByClassName("file_name");
 	for(k in files)	{
 		if (files[k].href != null) {
-      var params = getQueryStringArgs(files[k].href);
-      var name;
-      for (name in params) {
-        if (typeof params[name] !== "function") {
-          urls += name + ":" + params[name] + "<br />";
-        }
-      }
-			//urls += files[k].href + "\n";
+			var qs = files[k].href;
+			
+			var items = qs.split("&");
+
+			var item = null, name = null, value = null, header = null;
+			
+			for(var i=0; i < items.length; i++){
+				//urls += items[i] + "\n\n";
+
+				item = items[i].split("=");
+				name = item[0];
+				value = item[1];
+				
+				
+				if(name == "mu") continue;
+				if(name == "d") {
+					header = value;
+					urls += "&f=lixian.vip.xunlei.com";
+					continue;
+				}
+				
+				if( i > 0 )
+					urls += "&" + name + "=" + value;
+				else
+					urls += name + "=" + value;
+				
+			}
+			
+			var temp1 = "http://" + header + ".sendfile.vip.xunlei.com";
+			var start = urls.indexOf(":8000");
+			var temp2 = urls.substr(start);
+			urls = temp1 + temp2;
+		}
     }
     return urls;
 }
